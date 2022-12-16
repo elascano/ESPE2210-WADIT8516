6f677908fec78758dd3bf9293f7d2effd3df2eb8
Washington Alejandro Pilligua Chiliguano <wapilligua@espe.edu.ec>
2022-12-15 20:30:34 -0500

    First Partial Test

---
+++ exams/pilligua/U1/API_Students_REST/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const studentRoute = require("./routes/student");
+
+// settings
+const app = express();
+const port = process.env.PORT || 3015;
+
+// middlewares
+app.use(express.json());
+app.use("/api", studentRoute);
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Welcome to my API");
+});
+
+// mongodb connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
+
+// server listening
+app.listen(port, () => console.log("Server listening to", port));
